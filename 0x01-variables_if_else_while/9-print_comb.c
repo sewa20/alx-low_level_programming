14e3e529b98da21f466586c9bbbad07c34413f4f
sewa20 <mariam.adedeji03@gmail.com>
2022-09-10 13:19:32 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /** 
- * main - print single-digit numbers
+ * main - printing numbers from 0-9 with commas and space between them
  *
  * Description: using the main function
- * this program prints "single-digit numbers
+ * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
  * Return: 0
  */
 int main(void)
