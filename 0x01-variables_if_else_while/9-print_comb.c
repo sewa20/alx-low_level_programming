bbc763ec5b5978038ec7b310ead8eb108ec1e1ea
sewa20 <mariam.adedeji03@gmail.com>
2022-09-10 13:09:17 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/** 
+ * main - print single-digit numbers
+ *
+ * Description: using the main function
+ * this program prints "single-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
