4ad3cbb95db9300202bf3c7b2fc4342e389cfa2e
sewa20 <mariam.adedeji03@gmail.com>
2022-09-12 19:17:58 -0700

    Comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ for (q = p + 1; q <= 99; q++)
 putchar((p / 10) + '0');
 putchar((p % 10) + '0');
 putchar(' ');
-putchar((q / 10) + '0'):
+putchar((q / 10) + '0');
 putchar((q % 10) + '0');
 if (p == 98 && q == 99)
 continue;
