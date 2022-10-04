861926783d13a1ea66c5a089e0661d75ed3c7bcf
sewa20 <mariam.adedeji03@gmail.com>
2022-10-04 05:50:21 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,9 +10,8 @@
 int main(int argc, char **argv)
 {
 int num1, num2, result;
-
 if (argc != 3)
-{	
+{
 printf("Error\n");
 return (1);
 }
@@ -20,7 +19,7 @@ else
 {
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-result = num1 * num2;
+result = num1 *num2;
 printf("%d\n", result);
 return (0);
 }
