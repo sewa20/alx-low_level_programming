7e6120fc1442946c85db299ce770b4942cd1fedd
sewa20 <mariam.adedeji03@gmail.com>
2022-10-11 16:43:11 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,8 +17,8 @@ else
 printf("Name: (nil)\n");
 printf("Age: %.6f\n", d->age);
 if (d->owner != NULL)
-printf("owner: %s\n", d->owner);
+printf("Owner: %s\n", d->owner);
 else
-printf("owner: (nil)\n");
+printf("Owner: (nil)\n");
 }
 }
